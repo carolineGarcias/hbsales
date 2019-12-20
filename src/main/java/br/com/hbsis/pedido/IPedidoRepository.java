302bc56f6dfcfc90f8b93509b500ecf90f1216b2
carolineGarcias <carolinegarciasrocha@hotmail.com>
2019-12-20 15:10:02 -0300

    Pedido

--- src/main/java/br/com/hbsis/pedido/IPedidoRepository.java
+++ src/main/java/br/com/hbsis/pedido/IPedidoRepository.java
@@ -3,14 +3,13 @@ package br.com.hbsis.pedido;
 import br.com.hbsis.funcionarios.Funcionario;
 import br.com.hbsis.vendas.Vendas;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-
 
 public interface IPedidoRepository extends JpaRepository<Pedido, Long> {
 
     //List<Pedido> PedidoVendas(Vendas vendas);
     List<Pedido> findByFuncionario(Funcionario funcionario);
-
     boolean existsByCodPedido(String codPedido);
+
+    List<Pedido> findByVendas(Vendas vendas);
 }
